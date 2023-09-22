a1eb04d08dfd8aa57c6e04e9443bc2ea62c43525
andreslopezma <andres.lopezma@hotmail.com>
2023-09-22 12:10:56 -0500

    Se crea el modelo Quotations

---
+++ app/Models/ProductsQuotations.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProductsQuotations extends Model
+{
+    use HasFactory;
+
+    /**
+     * @var array
+     */
+    protected $fillable = ['product_id', 'quotation_id'];
+}
