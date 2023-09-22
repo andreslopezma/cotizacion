de878543899183601aea6e6fdb260f310c039ecb
andreslopezma <andres.lopezma@hotmail.com>
2023-09-22 18:04:36 -0500

    Se agrega la conexion de muchos a muchos entre la tabla products y Quotations

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Product extends Model
 {
@@ -13,4 +14,12 @@ class Product extends Model
      * @var array
      */
     protected $fillable = ['active', 'product_code', 'product_image', 'product_name', 'product_price'];
+
+    /**
+     * Get the Quotations
+     */
+    public function quotations(): BelongsToMany
+    {
+        return $this->belongsToMany(Quotation::class, 'products_quotations')->withPivot('value_total');
+    }
 }
